6eee0ed2c6ddf0ff455a4878fea1736c39e6ff6a
parvez alam <alamparvez301@gmail.com>
2019-02-03 15:31:41 +0530

    Add Property Detail Model

--- property_search/models.py
+++ property_search/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class PropertyDetail(models.Model):
+    """Property Model
+    """
+    lat = models.CharField(max_length=250)
+    lng = models.CharField(max_length=250)
+    bedroom = models.IntegerField(default=0)
+    bathrooms = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
